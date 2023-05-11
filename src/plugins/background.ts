b1e25c05dbc866826e498afe897a215139e0210e
Novout <novout@hotmail.com>
2023-05-10 23:09:10 -0300

    chore(runner): set background ignore

--- src/plugins/background.ts
+++ src/plugins/background.ts
@@ -5,8 +5,7 @@ export const runPluginBackground = async (instance: pdfeasy) => {
   if (instance.options?.plugins) {
     for (const plugin of instance.options.plugins) {
       if (plugin.background && instance.globals.__NEW_PAGE__) {
-        // @ts-ignore
-        const res = plugin.background(instance.pdfkit.page)
+        const res = plugin.background(instance.pdfkit!.page)
 
         if (res) await setBackground(instance, res)
       }
