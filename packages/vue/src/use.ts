36b8940bf93b571ac5c28e282cc85d75d084313f
Novout <novout@hotmail.com>
2023-05-18 12:41:30 -0300

    chore(vue): instance alert

--- packages/vue/src/use.ts
+++ packages/vue/src/use.ts
@@ -2,8 +2,15 @@ import pdfeasy from 'pdfeasy';
 import { getCurrentInstance, ComponentInternalInstance } from 'vue-demi';
 
 export const usePDF = (): typeof pdfeasy => {
-  const internalInstance = getCurrentInstance()
-  const pdf = (internalInstance as ComponentInternalInstance).appContext
+  const instance = getCurrentInstance();
+
+  if (!instance) {
+    console.warn(
+      "[VUE-PDFEASY] - Vue instance not exists. Hook is in setup() context?"
+    );
+  }
+
+  const pdf = (instance as ComponentInternalInstance).appContext
     .config.globalProperties.$pdf
 
   return pdf
